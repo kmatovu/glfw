8b33063b38682f425e61c29e11fb420120a76b6a
Kevin Matovu <kmatovu@gmail.com>
2019-07-14 12:07:00 -0400

    update gitignore and premake file

--- premake5.lua
+++ premake5.lua
@@ -7,21 +7,37 @@ project "GLFW"
 
   files
   {
-	"src/wl_init.c",
-	"src/linux_joystick.c",
-	"src/wl_monitor.c",
-	"src/posix_time.c",
-	"src/posix_thread.c",
-	"src/wl_window.c",
-	"src/egl_context.c",
-	"src/osmesa_context.c"
+    "include/GLFW/glfw3.h",
+    "include/GLFW/glfw3native.h",
+    "src/glfw_config.h",
+    "src/context.c",
+    "src/init.c",
+    "src/input.c",
+    "src/monitor.c",
+    "src/vulkan.c",
+    "src/window.c",
+    "src/xkb_unicode.c"
   }
 
-  defines
-  {
-	  "_GLFW_WAYLAND",
-	  "_CRT_SECURE_NO_WARNINGS"
-  }
+  filter "system:linux"
+    staticruntime "On"
+    pic "On"
 
-  filter { "system:windows", "configurations:Release" }
-    buildoptions "/MT"
+    files
+    {
+      "src/x11_init.c",
+      "src/linux_joystick.c",
+      "src/x11_monitor.c",
+      "src/posix_time.c",
+      "src/posix_thread.c",
+      "src/x11_window.c",
+      "src/egl_context.c",
+      "src/glx_context.c",
+      "src/osmesa_context.c"
+    }
+
+    defines
+    {
+      "_GLFW_X11",
+      "_CRT_SECURE_NO_WARNINGS"
+    }
