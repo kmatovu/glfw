279ac7145392e68e94a8ca23c62da5404b52d8b0
Kevin Matovu <kmatovu@gmail.com>
2019-07-14 22:22:25 -0400

    add windows dependencies

--- premake5.lua
+++ premake5.lua
@@ -41,3 +41,29 @@ project "GLFW"
       "_GLFW_X11",
       "_CRT_SECURE_NO_WARNINGS"
     }
+
+  filter "system:windows"
+    staticruntime "on"
+	  systemversion "latest"
+
+	files
+    {
+      "src/win32_init.c",
+      "src/win32_joystick.c",
+      "src/win32_monitor.c",
+      "src/win32_time.c",
+      "src/win32_thread.c",
+      "src/win32_window.c",
+	  "src/wgl_context.c",
+      "src/egl_context.c",
+      "src/osmesa_context.c"
+    }
+
+    defines
+    {
+      "_GLFW_WIN32",
+      "_CRT_SECURE_NO_WARNINGS"
+    }
+
+  filter { "system:windows", "configurations:Release" }
+    buildoptions "/MT"
